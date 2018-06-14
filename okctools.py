fa5197c8db58464033d20f32e7c13219120f9399
sdevan <stevendevan12@gmail.com>
2018-06-14 16:06:56 -0700

    Added python 3 compatibility to load_profiles_df and added word counting function.

--- okctools.py
+++ okctools.py
@@ -1,23 +1,41 @@
-# shebang?
-
-#
+import pickle as pkl
+import regex
 
 import pandas as pd
-import pickle as pkl
 
 
-def load_profiles_df(filename='profiles.pickle'):
+def load_profiles_df(filename='profiles.pickle', version='py2'):
+    """load_profiles_df(filename='profiles.pickle', py='py2')
+    Load and concatenate all DataFrame rows contained in the pickle file into
+    a single DataFrame. This must be done incrementally because the pickle was
+    written to incrementally as a safeguard against exceptions during web
+    scraping.
+    """
+
     df = pd.DataFrame()
-    profiles = open(filename, 'rb')
+    file = open(filename, 'rb')
 
     print('Loading file ...')
     try:
         while True:
-            temp = pkl.load(profiles)
+            if (version == 'py2'):
+                temp = pkl.load(file)
+            elif (version == 'py3'):
+                temp = pkl.load(file, encoding='latin1')
+            else:
+                print('Invalid version')
+
             df = df.append(temp, ignore_index=True)
     except EOFError:
         print('Finished loading file.')
     finally:
-        profiles.close()
+        file.close()
 
     return df
+
+
+def count_words(listofdicts):
+    words = 0
+    for essay in listofdicts:
+        words += len(regex.findall(r'\b\w+\b', essay['content']))
+    return words
